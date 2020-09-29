686bce96abc3afa2e5ebff906bb938adb39dce12
nsrau <new.urbanetz@gmail.com>
2020-09-30 01:17:51 +0200

    Updates

---
+++ precache-manifest.547ad704a6b349a77eeb4d640d10de98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b3115759783cdadb1198a026156200",
+    "url": "/understanding-hooks/index.html"
+  },
+  {
+    "revision": "8b76d3c65883f5e0bc5c",
+    "url": "/understanding-hooks/static/css/main.1b9275fe.chunk.css"
+  },
+  {
+    "revision": "9ef8078270ebdd1aaf0f",
+    "url": "/understanding-hooks/static/js/2.1f0d38ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/understanding-hooks/static/js/2.1f0d38ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b76d3c65883f5e0bc5c",
+    "url": "/understanding-hooks/static/js/main.3d859f4c.chunk.js"
+  },
+  {
+    "revision": "c2e3018207f8dc53e1cf",
+    "url": "/understanding-hooks/static/js/runtime-main.e7ceb0b9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4eb866ed9d263730adeb013da98ed3da.js"
+  "/understanding-hooks/precache-manifest.547ad704a6b349a77eeb4d640d10de98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/understanding-hooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
