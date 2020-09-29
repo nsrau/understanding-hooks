d827e4852d95aa23869a350f13dc58b6f958a58d
nsrau <new.urbanetz@gmail.com>
2020-09-30 01:14:40 +0200

    Updates

---
+++ precache-manifest.4eb866ed9d263730adeb013da98ed3da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ecd7da202d05ffe352b2b290fa255f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b76d3c65883f5e0bc5c",
+    "url": "/static/css/main.1b9275fe.chunk.css"
+  },
+  {
+    "revision": "9ef8078270ebdd1aaf0f",
+    "url": "/static/js/2.1f0d38ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1f0d38ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b76d3c65883f5e0bc5c",
+    "url": "/static/js/main.3d859f4c.chunk.js"
+  },
+  {
+    "revision": "57c4672d17eb5efad2b0",
+    "url": "/static/js/runtime-main.1c017a69.js"
+  }
+]);
